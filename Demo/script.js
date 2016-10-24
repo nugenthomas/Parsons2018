136d462611bf8c36f66ed99b07d49a99cc3dd6df
nugenthomas <thomasnguyen@Thomass-MacBook-Pro.local>
2016-10-24 09:04:55 -0400

    groupwork

---
+++ Demo/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $(".button1").click(function(){
+      $(".img1").toggle('show');
+  });
+
+  $(".button2").click(function(){
+      $(".img2").toggle('show');
+  });
+
+  $(".button3").click(function(){
+      $(".img3").toggle('show');
+  });
+
+  $(".button4").click(function(){
+      $(".img4").toggle('show');
+  });
+
+  $(".button5").click(function(){
+      $(".img5").toggle('show');
+  });
+
+  $(".button6").click(function(){
+      $(".img6").toggle('show');
+  });
+
+  $(".button7").click(function(){
+      $(".img7").toggle('show');
+  });
+
+  $(".button8").click(function(){
+      $(".img8").toggle('show');
+  });
+
+  $(".button9").click(function(){
+      $(".img9").toggle('show');
+  });
+
+  $(".button10").click(function(){
+      $(".img10").toggle('show');
+  });
+
+});
